a551c18d6d78cc88e7568e6b031153bdc438696b
Shiv Ansal <ansalshiv@gmail.com>
2017-07-30 05:50:42 -0700

    add Yes/No Intent

--- alexa_logic.py
+++ alexa_logic.py
@@ -13,8 +13,10 @@ def get_question():
 	sess = requests.Session()
 	html = sess.get('https://angelhacks17-nihaleg.c9users.io/cuisine')
 	data = json.loads(html.content.decode("utf-8"))
-	
-	return "You seem pretty hungry. Should I buy you" + str(data.get("name")) + "for "+ str(data.get("basePrice"))+"."
+	print(data)
+	meal = str((data.get("restaurants")[0]).get("name"))
+	# return "You seem pretty hungry. Should I buy you" + str(data.get("name")) + "for "+ str(data.get("basePrice"))+"."
+	return "Great. I think you might be in the mood for"+str(data.get("cuisine"))  +"food. I heard"+ str(data.get("name"))+ "has good ratings on yelp. I am going to order you"+ meal +"How does that sound?"
 
 @app.route("/")
 def homepage():
@@ -22,17 +24,21 @@ def homepage():
 
 @ask.launch
 def start_skill():
-	lol = get_question()
-	welcome_message = lol
+	welcome_message = "Hey there. It's a nice day outside! How are you doing?"
 	return question(welcome_message)
 
 @ask.intent("YesIntent")
 def order_food():
-	 return statement("COOL")
+	 return statement("Okay! I am moving the transaction to your phone! Please go confirm it!")
+
+@ask.intent("GetFoodIntent")
+def want_food():
+	lol = get_question()
+	return question(lol)
 
 @ask.intent("NoIntent")
 def no_intent():
-	bye = "Okay good bye!"
+	bye = "Okay, let me know then!"
 	return statement(bye)
 
 if __name__ == "__main__":
